bb95a460fcafe3aa66dd178dc4af9981b14b3d16
nikita <nikita@MacBook-Air-nikita.local>
2022-11-27 16:11:48 +0400

    about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react";
+import { Flex, Heading, Spacer } from "components";
 
 export const AboutPage: FC = () => {
   return (
-    <div>
-      <div>AboutPage</div>
-    </div>
+    <>
+      <Spacer spaceMob={52} spaceDesktop={52} />
+      <Flex alignItems="center" flexDirection="column">
+        <Heading isTextAlignCenter>Syomkin Project</Heading>
+      </Flex>
+    </>
   );
 };
